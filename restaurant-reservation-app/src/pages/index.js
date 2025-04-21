fcf249129547874f430e7a9701d15165fefc80ad
graceleedev <gracelee.dev@gmail.com>
2025-04-21 12:12:17 -0400

    Change file name in route path

--- restaurant-reservation-app/src/pages/index.js
+++ restaurant-reservation-app/src/pages/index.js
@@ -1,14 +1,17 @@
-// Grace test
+import Link from 'next/link';
+import styles from './index.module.css';
 
-import DetailPage from "./DetailPage";
-import Link from "next/link";
-
-export default function Home(params) {
-    return(
-        <div>
-            <h1>BistroTable</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <Link href={"./DetailPage"}>Book now</Link>
-        </div>
-    );
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+      <h1>Welcome to Our Reservation Service</h1>
+      <p>Find and book a table at your favorite restaurants.</p>
+      </div>
+      
+      <Link href="/restaurants">
+        <button className={styles.button}>Browse Restaurants</button>
+      </Link>
+    </div>
+  );
 }
