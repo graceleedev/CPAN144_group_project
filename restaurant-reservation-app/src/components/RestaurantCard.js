254320d8c1aa7a12c90753cf585e7c6f0453fc67
graceleedev <gracelee.dev@gmail.com>
2025-04-23 03:13:54 -0400

    remove redundant lines

--- restaurant-reservation-app/src/components/RestaurantCard.js
+++ restaurant-reservation-app/src/components/RestaurantCard.js
@@ -17,11 +17,9 @@ export default function RestaurantCard({ restaurant }) {
         <Link href={`/restaurant/${restaurant.id}`} className={styles.link}>
         
         <img src={restaurant.imageUrl} alt="" className={styles.img}></img>
-          {/* <button className={styles.button}>View Details & Reserve</button> */}
         </Link>
         </div>
         <h2 className={styles.h2}>{restaurant.name}</h2>
-        {/* <p className={styles.p}>Category: <strong>{restaurant.category}</strong> </p> */}
         <p className={styles.p}>Rating: {restaurant.rating} ⭐ ({restaurant.reviews} reviews)</p>
       </div>
   );
